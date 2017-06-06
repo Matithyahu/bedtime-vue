8dcf54ce6556f34cb3524ffd23863f8051a317d1
Matithyahu <matthew@metzger.cc>
2017-06-06 00:01:51 -0500

    Created html and js files and copied in code from my jsfiddle project

---
+++ bedtime.js
@@ -0,0 +1,23 @@
+var nightList = ["Dinner", "Wash face or Bath", "Brush Teeth", "Put on Pajamas", "Put away toys", "Storytime or read on your own", "Bedtime"]
+
+var myNumber = 0;
+
+var app1 = new Vue({
+  el: '#app-1',
+  data: {
+    message: nightList[myNumber]
+  },
+  methods: {
+    next: function () {
+      myNumber = myNumber + 1
+      this.message = nightList[myNumber]
+      if (myNumber > nightList.length - 1) {
+        this.message = "The End (really, go to bed!)"
+      }
+    },
+    reset: function () {
+      myNumber = 0
+      this.message = nightList[myNumber]
+    }
+  }
+})
