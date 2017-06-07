3da9a709f88a95bba8c8f60d72a065b5bcef9471
Matithyahu <matthew@metzger.cc>
2017-06-06 20:15:39 -0500

    Took a while to get working, but Vue is working now.

--- bedtime.js
+++ bedtime.js
@@ -1,23 +1,33 @@
-var nightList = ["Dinner", "Wash face or Bath", "Brush Teeth", "Put on Pajamas", "Put away toys", "Storytime or read on your own", "Bedtime"]
+var nightList = [
+  "Dinner",
+  "Wash face or Bath",
+  "Brush Teeth",
+  "Put on Pajamas",
+  "Put away toys",
+  "Storytime or read on your own",
+  "Bedtime"
+];
 
 var myNumber = 0;
 
-var app1 = new Vue({
-  el: '#app-1',
-  data: {
-    message: nightList[myNumber]
-  },
-  methods: {
-    next: function () {
-      myNumber = myNumber + 1
-      this.message = nightList[myNumber]
-      if (myNumber > nightList.length - 1) {
-        this.message = "The End (really, go to bed!)"
-      }
+window.addEventListener('load', function () { // needs this to wait until DOM
+  var app1 = new Vue({
+    el: '#app-1',
+    data: {
+      message: nightList[myNumber]
     },
-    reset: function () {
-      myNumber = 0
-      this.message = nightList[myNumber]
+    methods: {
+      next: function () {
+        myNumber = myNumber + 1
+        this.message = nightList[myNumber]
+        if (myNumber > nightList.length - 1) {
+          this.message = "The End (really, go to bed!)"
+        }
+      },
+      reset: function () {
+        myNumber = 0
+        this.message = nightList[myNumber]
+      }
     }
-  }
+  });
 })
